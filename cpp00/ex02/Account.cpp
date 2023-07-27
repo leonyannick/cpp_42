4c80fd8059063e4115700a8bed204e61271f03fd
leonyannick <leon.yannik@web.de>
2023-07-27 18:40:35 +0200

    add missing timestamp. missing calculation in makeWithdrawal. rework timestamp for std::c++98

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -3,32 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lbaumann <lbaumann@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lbaumann <lbaumann@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:23:38 by lbaumann          #+#    #+#             */
-/*   Updated: 2023/07/26 18:50:59 by lbaumann         ###   ########.fr       */
+/*   Updated: 2023/07/27 18:37:36 by lbaumann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
 #include <iostream>
-#include <chrono>
-#include <ctime>
-#include <iomanip>
-#include <sstream>
-
-void	Account::_displayTimestamp( void ) {
-	auto currentTime = std::chrono::system_clock::now();
-    std::time_t currentTimeT = std::chrono::system_clock::to_time_t(currentTime);
-    std::tm* timeInfo = std::localtime(&currentTimeT);
-    std::stringstream formattedTime;
-    formattedTime << std::put_time(timeInfo, "%Y%m%d");
-    formattedTime << "_" << std::put_time(timeInfo, "%H%M%S");
-    std::string timeString = formattedTime.str();
-    std::string finalTimeFormat = "[" + timeString + "] ";
-    std::cout << finalTimeFormat;
-}
+#include <ctime> //for _displayTimestamp
+#include <iomanip> //for setw
 
 Account::Account( int deposit ) 
 	: _nbDeposits( 0 ), _nbWithdrawals( 0 ),
@@ -49,7 +35,34 @@ Account::~Account( void ) {
 	_nbAccounts--;
 }
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
+int	Account::getNbAccounts( void ) {
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void ) {
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void ) {
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void ) {
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void ) {
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
+}
 
 void Account::makeDeposit( int deposit ) {
 	_displayTimestamp();
@@ -68,20 +81,21 @@ void Account::makeDeposit( int deposit ) {
 }
 
 bool Account::makeWithdrawal( int withdrawal ) {
-
+	_displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";";
 	std::cout << "p_amount:" << _amount << ";";
-	std::cout << "withdrawal:" << withdrawal << ";";
 	
 	if (withdrawal > _amount) {
-		std::cout << "refused" << std::endl;
+		std::cout << "withdrawal:refused" << std::endl;
 		return (false);
 	}
 	
 	_totalNbWithdrawals++;
 	_nbWithdrawals++;
 	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
 	
+	std::cout << "withdrawal:" << withdrawal << ";";
 	std::cout << "amount:" << _amount << ";";
 	std::cout << "nb_withdrawals:" << _nbWithdrawals << std::endl;
 	return (true);
@@ -99,31 +113,17 @@ void	Account::displayStatus( void ) const {
 	std::cout << "withdrawals:" << _nbWithdrawals << std::endl;
 }
 
-int Account::_nbAccounts = 0;
-int Account::_totalAmount = 0;
-int Account::_totalNbDeposits = 0;
-int Account::_totalNbWithdrawals = 0;
+void	Account::_displayTimestamp( void ) {
+	std::time_t		currentTime;
+	std::tm*		localTime;
+	char			timeBuffer[80];
 
-int	Account::getNbAccounts( void ) {
-	return (_nbAccounts);
-}
-
-int	Account::getNbDeposits( void ) {
-	return (_totalNbDeposits);
-}
-
-int	Account::getTotalAmount( void ) {
-	return (_totalAmount);
-}
-
-int	Account::getNbWithdrawals( void ) {
-	return (_totalNbWithdrawals);
-}
-
-void	Account::displayAccountsInfos( void ) {
-	_displayTimestamp();
-	std::cout << "accounts:" << getNbAccounts() << ";";
-	std::cout << "total:" << getTotalAmount() << ";";
-	std::cout << "deposits:" << getNbDeposits() << ";";
-	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
+	// Get the current time
+	currentTime = std::time(NULL);
+	// Convert the current time to the local time
+	localTime = std::localtime(&currentTime);
+	// Format the local time as a string
+	std::strftime(timeBuffer, sizeof(timeBuffer), "[%Y%m%d %H%M%S] ", localTime);
+	// Print the formatted time
+	std::cout << timeBuffer;
 }
