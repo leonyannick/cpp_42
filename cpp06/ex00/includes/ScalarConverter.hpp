2d07aebff072a3cf3b9ed74ef1748137883da4a3
leonyannick <leon.yannik@web.de>
2023-11-16 19:01:13 +0100

    finish cpp06/ex00

---
+++ cpp06/ex00/includes/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <limits.h>
+#include <sstream>
+#include <cstring>
+#include <iomanip>
+
+class ScalarConverter
+{
+public:
+	// public methods
+	static void	convert(std::string literal);
+
+private:
+	static void	displayConversion(double d);
+	static void	display();
+	ScalarConverter(void);
+};
+
+#endif
