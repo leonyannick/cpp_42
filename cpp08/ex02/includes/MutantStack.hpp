e2fee15e9d824a64825f7e13623d3bcfe1031d01
leonyannick <leon.yannik@web.de>
2023-12-20 10:51:57 +0100

    finish cpp08/ex02

--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -5,9 +5,10 @@
 #include <algorithm>
 #include <stdexcept>
 #include <stack>
+#include <deque>
 
 template <typename T, typename Container = std::deque<T> >
-class MutantStack : public std::stack< T, Container>
+class MutantStack : public std::stack<T, Container>
 {
 public:
 	// public methods
@@ -15,15 +16,26 @@ public:
 	//Getters and Setters
 
 	// Constructors, Assignment, Destructors
-	MutantStack(void);
+	MutantStack(void) : std::stack<T, Container>() {};
 	
-	MutantStack(const MutantStack& copy);
-	MutantStack& operator=(const MutantStack& other);
-	~MutantStack(void);
+	MutantStack(const MutantStack& copy) {*this = copy;};
 
+	MutantStack& operator=(const MutantStack& other) {
+		std::stack<T, Container>::operator=(other);
+		return *this;
+	};
+
+	~MutantStack(void) {};
+
+	typedef typename Container::iterator iterator;
 	
+	//c is a member object of std::stack containing the underlaying container
+	//MutantStacks begin and end member function call begin and end of the
+	//underlaying container
+	iterator	begin() {return this->c.begin();};
+	iterator	end() {return this->c.end();};
 };
 
-std::ostream &		operator<<(std::ostream & o, MutantStack const & i);
+// std::ostream &		operator<<(std::ostream & o, MutantStack const & i);
 
 #endif
